44a3ec32b104fa4bf2721d81d497660fbf35dfb3
Dany Briceno <dany.briceno65@gmail.com>
2025-03-27 22:18:45 -0500

    something with turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['qctxtshwklpnvydtlsqg.supabase.co'],
       },
+      experimental: {
+        turbopack: true,
+      },
 };
 
 export default nextConfig;
